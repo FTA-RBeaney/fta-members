f335bd129a2e889f6c630424264c1fea63ba9557
rob-df <robert.beaney@digitalfrontier.com>
2025-04-02 10:49:33 +0100

    Added Tailwind and ShadCn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,28 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-  ],
+  modules: ["@nuxt/content", "shadcn-nuxt"],
+  css: ["~/assets/css/tailwind.css"],
+
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
   },
-  compatibilityDate: '2024-04-03',
-})
+  compatibilityDate: "2024-04-03",
+  modules: ["shadcn-nuxt"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./app/components/ui"
+     */
+    componentDir: "./app/components/ui",
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
